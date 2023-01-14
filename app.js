186ad99fb87f0e9886ca50aec65f7928ce04217c
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-01-14 11:55:09 +0100

    Readme added, comments added

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ const dotenv = require("dotenv").config(); // Gère les variables d'environnemen
 const path = require("path"); // Manipule les chemins de fichier
 
 const helmet = require("helmet"); // Sécurité : Configure les headers
-const cors = require("cors"); //
-const morgan = require("morgan");
-const mongooseExpressErrorHandler = require("mongoose-express-error-handler");
+const cors = require("cors"); // Sécurité : gère les accès serveur depuis des origines
+const morgan = require("morgan"); // Log les requêtes HTTP du serveur
+const mongooseExpressErrorHandler = require("mongoose-express-error-handler"); // Remonte les erreurs Mongoose et les gère de manière centralisée
 
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // Utilisation de Mongoose
 
-const userRoutes = require("./routes/user");
-const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user"); // Chemin user
+const sauceRoutes = require("./routes/sauce"); // Chemin sauce
 
-const app = express();
+const app = express(); // Lance Express sur Node.JS
 
+//  Configure l'app à la base de données Mongoose
 mongoose.set("strictQuery", true);
 mongoose
     .connect(`${process.env.MONGODBSRV}`, {
@@ -24,30 +25,45 @@ mongoose
     .then(() => console.log("Connexion à MongoDB réussie !"))
     .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use(express.json());
+app.use(express.json()); // Permet de gérer les données requête envoyées sous forme de JSON : parse les données et les ajoute à l'objet req.body
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    next();
-});
+// // Définit les entêtes de manière basique : inutile si Helmet est configuré
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//     );
+//     res.setHeader(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//     );
+//     next();
+// });
 
+// Ecoute les évenements de la BD et remonte les erreurs
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 
+// Définit les CORS sur une requête provenant du même site
 app.use(helmet({ crossOriginResourcePolicy: { policy: "same-site" } }));
+
+// Lance MongooseExpressErrorHandler
 app.use(mongooseExpressErrorHandler);
+
+// Définit l'utilisation de morgan avec un format de journalisation de manière détaillée
 app.use(morgan("combined"));
+
+// Utilise CORS fourni par Express
 app.use(cors());
+
+// Définit le chemin images
 app.use("/images", express.static(path.join(__dirname, "images")));
+
+// Impose l'authentification sur la route utilisateur en indiquant à l'app la route à suivre pour les requêtes /api/auth
 app.use("/api/auth", userRoutes);
-app.use("/api/sauces", sauceRoutes);
+
+// Indique à l'app la route à suivre pour les requêtes /api/sauces
+app.use("/api/sauces", sauceRoutes); //
 
 module.exports = app;
